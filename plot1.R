67e35a3b69020b966a578bc4e391cd86d6cc0782
mfindrik <mislav.findrik@gmail.com>
2019-08-27 17:12:24 +0200

    Update

---
+++ plot1.R
@@ -0,0 +1,48 @@
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from 
+# all sources for each of the years 1999, 2002, 2005, and 2008.
+
+# Read data into the variables
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# head(NEI)
+##     fips      SCC Pollutant Emissions  type year
+## 4  09001 10100401  PM25-PRI    15.714 POINT 1999
+## 8  09001 10100404  PM25-PRI   234.178 POINT 1999
+## 12 09001 10100501  PM25-PRI     0.128 POINT 1999
+## 16 09001 10200401  PM25-PRI     2.036 POINT 1999
+
+# str(SCC)
+## SCC                : Factor w/ 11717 levels "10100101","10100102",..: 1 2 3 4 5 6 7 8 9 10 ...
+## Data.Category      : Factor w/ 6 levels "Biogenic","Event",..: 6 6 6 6 6 6 6 6 6 6 ...
+## Short.Name         : Factor w/ 11238 levels "","2,4-D Salts and Esters Prod /Process Vents, 2,4-D Recovery: Filtration",..: 3283 3284 3293 3291 3290 3294 3295 3296 3292 3289 ...
+## EI.Sector          : Factor w/ 59 levels "Agriculture - Crops & Livestock Dust",..: 18 18 18 18 18 18 18 18 18 18 ...
+## Option.Group       : Factor w/ 25 levels "","C/I Kerosene",..: 1 1 1 1 1 1 1 1 1 1 ...
+## Option.Set         : Factor w/ 18 levels "","A","B","B1A",..: 1 1 1 1 1 1 1 1 1 1 ...
+## SCC.Level.One      : Factor w/ 17 levels "Brick Kilns",..: 3 3 3 3 3 3 3 3 3 3 ...
+## SCC.Level.Two      : Factor w/ 146 levels "","Agricultural Chemicals Production",..: 32 32 32 32 32 32 32 32 32 32 ...
+## SCC.Level.Three    : Factor w/ 1061 levels "","100% Biosolids (e.g., sewage sludge, manure, mixtures of these matls)",..: 88 88 156 156 156 156 156 156 156 156 ...
+## SCC.Level.Four     : Factor w/ 6084 levels "","(NH4)2 SO4 Acid Bath System and Evaporator",..: 4455 5583 4466 4458 1341 5246 5584 5983 4461 776 ...
+## Map.To             : num  NA NA NA NA NA NA NA NA NA NA ...
+## Last.Inventory.Year: int  NA NA NA NA NA NA NA NA NA NA ...
+## Created_Date       : Factor w/ 57 levels "","1/27/2000 0:00:00"
+## Revised_Date       : Factor w/ 44 levels "","1/27/2000 0:00:00"
+## Usage.Notes        : Factor w/ 21 levels ""," ","includes bleaching towers, washer hoods, filtrate tanks
+
+library("dplyr")
+
+year99 <- NEI %>% filter(year==1999)
+summary99 <- summary(year99$Emissions)
+
+year02 <- NEI %>% filter(year==2002)
+summary02 <- summary(year02)
+
+year05 <- NEI %>% filter(year==2005)
+summary05 <- summary(year05)
+
+year08 <- NEI %>% filter(year==2008)
+summary08 <- summary(year08)
+
+
+
